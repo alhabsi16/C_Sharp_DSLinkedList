98df1f92029e4516513f7765f1fb0080b3df77ef
alhabsi16 <mxmx8283@gmail.com>
2023-07-10 13:45:09 +0400

    linked list

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace C_Sharp_DSLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Insert(10);
+            linkedList.Insert(20);
+            linkedList.Insert(30);
+            linkedList.Insert(40);
+           // linkedList.Display();
+
+            //Console.WriteLine();
+            //Console.WriteLine("Display after Deleting");
+            //linkedList.Delete(40);
+            //linkedList.Display();
+
+            //Task: Add functionality to add item anywhere, given a position
+          //  Console.WriteLine();
+          //  Console.WriteLine("Display after add position ");
+            linkedList.insertNewNode(50,6);
+            linkedList.Display();
+
+            //Console.WriteLine();
+            //Console.WriteLine("Display after  ");
+           Console.WriteLine(linkedList.findData(40));
+        }
+    }
+}
